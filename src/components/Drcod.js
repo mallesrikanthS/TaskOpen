185a4d3dce717b2e60705a0f2373fb7bd1035996
mallesrikanthS <86652218+mallesrikanthS@users.noreply.github.com>
2021-07-19 18:56:42 +0530

    Update Drcod.js
    updated type of input

--- src/components/Drcod.js
+++ src/components/Drcod.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Icon } from '@fluentui/react/lib/Icon';
 
-import taskdata from '../data/taskdata.json';
+//import taskdata from '../data/taskdata.json';
 import './App.css';
 import Recod from './Recod';
 
@@ -16,63 +16,66 @@ class Drcod extends Component {
             return(
                 <>
                 {(
-                    ()=> {
-                        let posts = [];
-                        let daq = [];
-
-                        for (var id in this.props.data.arwval) {
-        
-                            if (st.key=== id) {
-                                posts.push(
-                                    <tr key={idval}>
-                                    <td><button id='but1' onClick= {()=> this.props.func(st.key)} >
-                                        <Icon iconName="DrillDownSolid" />
-                                        </button> {st.arr1}</td>
-                                    <td><input type='text'  name={st.LAD} onChange={(e)=> this.props.handleChange(e)} /></td>
-                                    <td><input type='text'  name={st.RAD} onChange={(e)=> this.props.handleRChange(e)} /></td>
-                                    </tr>
-                            
-                                )
-
-                            console.log('posts data', posts);
-
-                            //console.log('id check',this.props.data.arwval[id],id);
-
-                            {(
-                                ()=> {
-
-                                if(this.props.data.arwval[id] === true ) {
-                                console.log('id check',this.props.data.arwval[id]);
-
-                                daq.push(
-                                    <Recod st={st} data={this.props.data} 
-                                    handleChange={this.props.handleChange}
-                                    handleRChange={this.props.handleRChange}/>
-                                )                                                                               
-                            } 
-
-                            return daq;
-
-                            })
-
-                            ()}
-                            posts.push(daq);
-
-                        }
-
+                ()=> {
+                    let posts = [];
+                    let daq = [];
+                    for (var id in this.props.data.arwval) {
+    
+                        if (st.key=== id ) {
+                            if(st.Rdata){
+                            posts.push(
+                                <tbody key={idval}>
+                                <tr key={idval}>
+                                <td><button id='but1' onClick= {()=> this.props.func(st.key)} >
+                                    <Icon iconName="DrillDownSolid" />
+                                    </button> {st.arr1}</td>
+                                <td><input type='number'  name={st.LAD} onChange={(e)=> this.props.handleChange(e)} /></td>
+                                <td><input type='number'  name={st.RAD} onChange={(e)=> this.props.handleRChange(e)} /></td>
+                                </tr>
+                                </tbody>                            
+                            )
+                        //console.log('posts data', posts);
+                        //console.log('id check',this.props.data.arwval[id],id);
+                        {(
+                            ()=> {
+                            if(this.props.data.arwval[id] === true ) {
+                            //console.log('id check',this.props.data.arwval[id]);
+                            daq.push(
+                                <Recod st={st} data={this.props.data} 
+                                handleChange={this.props.handleChange}
+                                handleRChange={this.props.handleRChange}/>
+                            )  }                                                                             
+                                
+                            //return daq;
+                        })
+                        ()}
                         
-                        };
-                        return posts;
                     }
-                )
-                
-                ()}
-
-
-            </>
-
+                    else {
+                        posts.push(
+                            <tbody key={idval}>
+                            <tr key={idval}>
+                            <td id='but1'> {st.arr1}</td>
+                            <td><input type='number'  name={st.LAD} onChange={(e)=> this.props.handleChange(e)} /></td>
+                            <td><input type='number'  name={st.RAD} onChange={(e)=> this.props.handleRChange(e)} /></td>
+                            </tr>
+                            </tbody>                            
+                        )
+                    }
+                    posts.push(daq);
+                    }
+                    //posts.push(daq);   
+                    };
+                    return posts;
+                }
             )
-            })}
+                
+            ()}       
+
+        </>
+
+        )
+        })}
             
 
 
@@ -83,17 +86,4 @@ class Drcod extends Component {
 
 
 export default Drcod;
-
-
-
-
-
-
-
-
-
-
-
-
-
 
