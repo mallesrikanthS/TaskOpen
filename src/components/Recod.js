83c1e996ecaecb4062f3d9fe365db08dd0281da5
mallesrikanthS <86652218+mallesrikanthS@users.noreply.github.com>
2021-07-19 18:57:26 +0530

    Update Recod.js
    updated input type

--- src/components/Recod.js
+++ src/components/Recod.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import taskdata from '../data/taskdata.json';
+//import taskdata from '../data/taskdata.json';
 import './App.css';
 
 
@@ -8,23 +8,21 @@ class Recod extends Component {
     render(){
         //console.log(this.props.handleChange);
         return(
-            <>
-            
+            <>            
             {this.props.st.Rdata.map((rt,id)=> {
                 //console.log(rt.DL);
                 return(
+                    <tbody key={id}>
                     <tr key={id}>
                         <td id='nbut'>{rt.rd}</td>
-                        <td><input type='text'  name={rt.DL} onChange={(e)=> this.props.handleChange(e)} /></td>
-                        <td><input type='text'  name={rt.DR} onChange={(e)=> this.props.handleRChange(e)} /></td>
-                        
+                        <td><input type='number'  name={rt.DL} onChange={(e)=> this.props.handleChange(e)} /></td>
+                        <td><input type='number'  name={rt.DR} onChange={(e)=> this.props.handleRChange(e)} /></td>                        
                     </tr>
+                    </tbody>
                     ) 
             })}
-
             </>
         )
-
     }
 }
 
